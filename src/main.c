753040e776ad127ac2ee19a7a2e881d8473aa4a6
renkon <takatsugu0404@gmail.com>
2025-11-05 17:22:36 +0900

    refactor

--- src/main.c
+++ src/main.c
@@ -7,27 +7,7 @@
 Token *token;
 Node *functions[100];
 
-int main(int argc, char **argv) {
-    if (argc != 2) {
-        error("Usage: 9cc <filename>");
-    }
-
-    char *user_input = read_file(argv[1]);
-    token = tokenize(user_input);
-
-    program();
-
-    printf(".intel_syntax noprefix\n");
-    printf(".section .note.GNU-stack,\"\",@progbits\n");
-
-    printf(".data\n");
-    for (StringLiteral *str = string_literals; str; str = str->next) {
-        static int str_count = 0;
-        str->id = str_count;
-        printf(".LC%d:\n", str_count++);
-        printf("  .string \"%s\"\n", str->contents);
-    }
-
+void gen_Gvar() {
     for (GVar *gvar = globals; gvar; gvar = gvar->next) {
         printf("%s:\n", gvar->name);
         int sz = size_of(gvar->type);
@@ -51,6 +31,34 @@ int main(int argc, char **argv) {
             printf("  .zero %d\n", sz);
         }
     }
+}
+
+void gen_StringLiteral() {
+    for (StringLiteral *str = string_literals; str; str = str->next) {
+        static int str_count = 0;
+        str->id = str_count;
+        printf(".LC%d:\n", str_count++);
+        printf("  .string \"%s\"\n", str->contents);
+    }
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        error("Usage: 9cc <filename>");
+    }
+
+    char *user_input = read_file(argv[1]);
+    token = tokenize(user_input);
+
+    program();
+
+    printf(".intel_syntax noprefix\n");
+    printf(".section .note.GNU-stack,\"\",@progbits\n");
+
+    printf(".data\n");
+
+    gen_Gvar();
+    gen_StringLiteral();
 
     printf(".text\n");
     printf(".global main\n");
